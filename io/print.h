8b66992e553658bcab61be399b87a8996b521de4
PetrFlajsingr <flajsingr@gmail.com>
2019-11-17 22:05:24 +0100

    Initial commit

---
+++ io/print.h
@@ -0,0 +1,55 @@
+//
+// Created by petr on 11/16/19.
+//
+
+#ifndef TERRAINGENERATION_PRINT_H
+#define TERRAINGENERATION_PRINT_H
+
+#include <iostream>
+#include "logger.h"
+
+namespace detail {
+template <typename OutStream, typename ...Args>
+void printImpl(OutStream &stream, Args... args) {
+  using namespace LoggerStreamModifiers;
+  static auto logger = Logger{stream};
+  ((logger << verbose()) << ... << args) << "\n";
+}
+
+template<typename OutStream>
+class CustomPrint {
+     OutStream &stream;
+  public:
+    explicit CustomPrint(OutStream &stream) : stream(stream) {}
+
+    template <typename ...Args>
+    void operator()(Args ...args) {
+      printImpl(stream, args...);
+    }
+  };
+}
+template <typename ...Args>
+void print(Args... args) {
+  detail::printImpl(std::cout, std::forward<Args>(args)...);
+}
+
+template <typename ...Args>
+void printErr(Args... args) {
+  detail::printImpl(std::cerr, std::forward<Args>(args)...);
+}
+
+template <typename OutStream>
+auto make_print(OutStream &stream) {
+  return detail::CustomPrint{stream};
+}
+
+/*
+template <typename OutStream>
+auto make_print(OutStream &stream) {
+  return [&stream]<typename ...Args> (Args... args) {
+    detail::printImpl(stream, std::forward<Args>(args)...);
+  };
+}
+*/
+
+#endif // TERRAINGENERATION_PRINT_H

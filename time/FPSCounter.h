5d0d0971d783623de79b8921b53af5f03dee37f0
Petr <flajsingr@gmail.com>
2019-11-19 15:36:03 +0100

    add FPCounter

---
+++ time/FPSCounter.h
@@ -0,0 +1,35 @@
+//
+// Created by Petr on 19.11.2019.
+//
+
+#ifndef UTILITIES_FPSCOUNTER_H
+#define UTILITIES_FPSCOUNTER_H
+
+#include <chrono>
+#include <ostream>
+#include "now.h"
+
+using namespace std::chrono_literals;
+
+class FPSCounter {
+public:
+    [[nodiscard]] float average() const;
+
+    [[nodiscard]] float current() const;
+
+    void frame();
+
+    void restart();
+
+    friend std::ostream &operator<<(std::ostream &stream, const FPSCounter &fpsCounter);
+
+private:
+    uint64_t totalFrameCount = 0;
+    std::chrono::milliseconds totalTimeSum = 0ms;
+    std::chrono::milliseconds lastTimeStamp = now<std::chrono::milliseconds>();
+    std::chrono::milliseconds lastFrameDuration = 0ms;
+
+};
+
+
+#endif //UTILITIES_FPSCOUNTER_H

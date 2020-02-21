490b5568bd758b6263ba926f390d83e560a7f9d3
Petr <flajsingr@gmail.com>
2020-02-21 12:10:56 +0100

    add byte literals for IEC prefixes

--- various/ByteLiterals.h
+++ various/ByteLiterals.h
@@ -6,13 +6,22 @@
 #define UTILITIES_BYTELITERALS_H
 
 namespace ByteLiterals {
-constexpr auto orderStep = 1000;
+namespace detail {
+constexpr auto byteOrderMultiplier = 1000;
+constexpr auto biByteOrderMultiplier = 1024;
+} // namespace detail
 constexpr unsigned long long operator"" _B(unsigned long long value) { return value; }
-constexpr unsigned long long operator"" _KB(unsigned long long value) { return value * orderStep; }
-constexpr unsigned long long operator"" _MB(unsigned long long value) { return value * std::pow(orderStep, 2); }
-constexpr unsigned long long operator"" _GB(unsigned long long value) { return value * std::pow(orderStep, 3); }
-constexpr unsigned long long operator"" _TB(unsigned long long value) { return value * std::pow(orderStep, 4); }
-constexpr unsigned long long operator"" _PB(unsigned long long value) { return value * std::pow(orderStep, 5); }
+constexpr unsigned long long operator"" _KB(unsigned long long value) { return value * byteOrderMultiplier; }
+constexpr unsigned long long operator"" _MB(unsigned long long value) { return value * std::pow(byteOrderMultiplier, 2); }
+constexpr unsigned long long operator"" _GB(unsigned long long value) { return value * std::pow(byteOrderMultiplier, 3); }
+constexpr unsigned long long operator"" _TB(unsigned long long value) { return value * std::pow(byteOrderMultiplier, 4); }
+constexpr unsigned long long operator"" _PB(unsigned long long value) { return value * std::pow(byteOrderMultiplier, 5); }
+
+constexpr unsigned long long operator"" _KiB(unsigned long long value) { return value * biByteOrderMultiplier; }
+constexpr unsigned long long operator"" _MiB(unsigned long long value) { return value * std::pow(biByteOrderMultiplier, 2); }
+constexpr unsigned long long operator"" _GiB(unsigned long long value) { return value * std::pow(biByteOrderMultiplier, 3); }
+constexpr unsigned long long operator"" _TiB(unsigned long long value) { return value * std::pow(biByteOrderMultiplier, 4); }
+constexpr unsigned long long operator"" _PiB(unsigned long long value) { return value * std::pow(biByteOrderMultiplier, 5); }
 
 } // namespace ByteLiterals
 

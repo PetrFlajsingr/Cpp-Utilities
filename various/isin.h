a934ca622bf73e9f48b5e97a749289e62bc7f058
Petr <flajsingr@gmail.com>
2020-02-21 12:07:32 +0100

    add default Container type for isIn as std::initialiser_list to avoid unnecessary template specialisation

--- various/isin.h
+++ various/isin.h
@@ -5,11 +5,7 @@
 #ifndef UTILITIES_ISIN_H
 #define UTILITIES_ISIN_H
 
-template <typename T, typename Container> bool isIn(T value, Container &&container) {
-  return std::find_if(container.begin(), container.end(), [value](const T &val) { return val == value; }) != container.end();
-}
-
-template <typename T> bool isIn(T value, std::initializer_list<T> &&container) {
+template <typename T, typename Container = std::initializer_list<T>> bool isIn(T value, Container &&container) {
   return std::find_if(container.begin(), container.end(), [value](const T &val) { return val == value; }) != container.end();
 }
 
